41ce1a0d24e718e91eddd38a296abe004f42e34e
cindytsai <6188366+cindytsai@users.noreply.github.com>
2020-12-28 22:05:41 +0800

    Update frontends/libyt

--- yt_libyt/data_structures.py
+++ yt_libyt/data_structures.py
@@ -18,6 +18,7 @@ import stat
 import time
 import numpy as np
 import weakref
+from collections import defaultdict
 
 from yt.funcs import \
     mylog, \
@@ -33,6 +34,9 @@ from yt.utilities.file_handler import \
 from yt.utilities.parameter_file_storage import \
     output_type_registry
 from .fields import libytFieldInfo
+from yt.geometry.geometry_handler import YTDataChunk
+
+import inspect
 
 
 class libytGrid(AMRGridPatch):
@@ -42,6 +46,14 @@ class libytGrid(AMRGridPatch):
         AMRGridPatch.__init__(self, id,
                               filename=None,
                               index=index)
+
+        # TODO:
+        # Might be redundant by setting these two, 
+        # should revisit when we consider load balance.
+        # 
+        # MPI_rank : Refers to grid is currently belongs to such MPI rank.
+        # filename : Refers to grid originally belongs to which MPI rank.
+        self.MPI_rank = None
         self.Parent = None
         self.Children = []
         self.Level = level
@@ -81,6 +93,9 @@ class libytHierarchy(GridIndex):
         self.num_grids = self.libyt.param_yt['num_grids']
 
     def _parse_index(self):
+
+        mylog.debug("#FLAG#")
+        mylog.debug("yt/frontends/libyt/data_structures.py (class libytHierarchy, def _parse_index)")
         # hierarchy information of all grids
         hierarchy = self.libyt.hierarchy
 
@@ -93,16 +108,30 @@ class libytHierarchy(GridIndex):
         self.grid_particle_count[:] = hierarchy['grid_particle_count'][:]
         self.max_level = self.grid_levels.max()
 
+        # Indicates which OpenMPI rank it belongs to.
+        self.proc_num = hierarchy['proc_num'].copy()
+
+        mylog.debug("grid_left_edge = %s", self.grid_left_edge)
+        mylog.debug("grid_right_edge = %s", self.grid_right_edge)
+        mylog.debug("grid_dimensions = %s", self.grid_dimensions)
+        mylog.debug("grid_levels = %s", self.grid_levels)
+        mylog.debug("grid_particle_count = %s", self.grid_particle_count)
+        mylog.debug("proc_num = %s", self.proc_num)
+
         # allocate all grid objects
         self.grids = np.empty(self.num_grids, dtype='object')
         for i in range(self.num_grids):
             self.grids[i] = self.grid(i, self, self.grid_levels.flat[i])
+            self.grids[i].MPI_rank = self.proc_num[i,0]
+            self.grids[i].filename = "MPI_Rank_%07i" % (self.proc_num[i,0])
 
         # calculate the starting particle indices for each grid (starting from 0)
         # --> note that the last element must store the total number of particles
         #    (see _read_particle_coords and _read_particle_fields in io.py)
         # self._particle_indices = np.zeros(self.num_grids + 1, dtype='int64')
         # np.add.accumulate(self.grid_particle_count.squeeze(), out=self._particle_indices[1:])
+        # 
+        mylog.debug("######")
 
     def _populate_grid_objects(self):
         # must flat it since 'grid_parent_id' has the dimension [num_grids][1]
@@ -134,19 +163,51 @@ class libytHierarchy(GridIndex):
 #       mylog.info('Validating the parent-children relationship ...')
 #       mylog.info('Check passed')
 
-### NOT SURE IF WE NEED THIS (see EnzoHierarchyInMemory class)
-#   def _chunk_io(self, dobj, cache = True, local_only = False):
-#       gfiles = defaultdict(list)
-#       gobjs = getattr(dobj._current_chunk, "objs", dobj._chunk_info)
-#       for g in gobjs:
-#           gfiles[g.filename].append(g)
-#       for fn in sorted(gfiles):
-#           if local_only:
-#               gobjs = [g for g in gfiles[fn] if g.proc_num == self.comm.rank]
-#               gfiles[fn] = gobjs
-#           gs = gfiles[fn]
-#           count = self._count_selection(dobj, gs)
-#           yield YTDataChunk(dobj, "io", gs, count, cache = cache)
+    def _chunk_io(self, dobj, cache = True, local_only = False):
+
+        mylog.debug("#FLAG#")
+        mylog.debug("yt/frontends/libyt/data_structures.py (class libytHierarchy, def _chunk_io())")
+
+        gfiles = defaultdict(list)
+
+        mylog.debug("dobj._current_chunk = %s", dobj._current_chunk)
+        mylog.debug("dobj._chunk_info = %s", dobj._chunk_info)
+        mylog.debug("dobj type = %s", type(dobj))
+        mylog.debug("dobj._current_chunk type = %s", type(dobj._current_chunk))
+        mylog.debug("dobj._chunk_info type = %s", type(dobj._chunk_info))
+
+        mylog.debug("dobj._current_chunk attributes: ")
+        mylog.debug("%s", inspect.getmembers(dobj._current_chunk, lambda a: not (inspect.isroutine(a))))
+
+        gobjs = getattr(dobj._current_chunk, "objs", dobj._chunk_info)
+
+        mylog.debug("gobjs = %s", gobjs)
+        mylog.debug("gobjs type = %s", type(gobjs))
+
+        for g in gobjs:
+            mylog.debug("g.filename = %s", g.filename)
+            gfiles[g.filename].append(g)
+
+        mylog.debug("gfiles = %s", gfiles)
+
+        for fn in sorted(gfiles):
+            # TODO: Check the "local_only"
+            if local_only:
+
+                for g in gfiles[fn] :
+                    mylog.debug("g = %s, g.MPI_rank = %s, self.comm.rank = %s" % (g, g.MPI_rank, self.comm.rank))
+
+                gobjs = [g for g in gfiles[fn] if g.MPI_rank == self.comm.rank]
+                gfiles[fn] = gobjs 
+            gs = gfiles[fn]
+
+            mylog.debug("filter out gfiles[fn] = %s", gs)
+
+            count = self._count_selection(dobj, gs)
+
+            mylog.debug("######(class libytHierarchy, def _chunk_io())")
+            
+            yield YTDataChunk(dobj, "io", gs, count, cache = cache)
 
 
 class libytDataset(Dataset):
