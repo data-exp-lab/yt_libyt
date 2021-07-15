605d242bb3696b7fa4195a73ed1e2f5e1fe2e060
cindytsai <6188366+cindytsai@users.noreply.github.com>
2021-07-15 22:19:21 +0800

    Update comments.

--- yt_libyt/io.py
+++ yt_libyt/io.py
@@ -76,8 +76,8 @@ class IOHandlerlibyt(BaseIOHandler):
                     y = self.libyt.get_attr(g.id, ptype, coor_label[1])
                     z = self.libyt.get_attr(g.id, ptype, coor_label[2])
 
-                    # if ptype particle num in grid g.id = 0, get_attr will return None.
-                    # then we shall continue the loop
+                    # g.id ptype particle number is 0 or g.id is not in local grids
+                    # libyt.get_attr will return None
                     if x is None or y is None or z is None:
                         continue
                     else:
@@ -102,6 +102,11 @@ class IOHandlerlibyt(BaseIOHandler):
                     y = self.libyt.get_attr(g.id, ptype, coor_label[1])
                     z = self.libyt.get_attr(g.id, ptype, coor_label[2])
 
+                    # g.id ptype particle number is 0 or g.id is not in local grids
+                    # libyt.get_attr will return None
+                    if x is None or y is None or z is None:
+                        continue
+
                     mask = selector.select_points(x, y, z, 0.0)
                     if mask is None:
                         continue
@@ -110,6 +115,8 @@ class IOHandlerlibyt(BaseIOHandler):
                         data = self.libyt.get_attr(g.id, ptype, field)
                         # if ptype particle num in grid g.id = 0, get_attr will return None.
                         # then we shall continue the loop
+                        # g.id ptype particle number is 0 or g.id is not in local grids
+                        # libyt.get_attr will return None
                         if data is None:
                             continue
                         else:
