dba379fa3dc7185cd0d78f30d8cd2d5841ee4e92
cindytsai <6188366+cindytsai@users.noreply.github.com>
2021-09-02 00:19:20 +0800

    Update frontendslibyt/io.py

--- yt_libyt/io.py
+++ yt_libyt/io.py
@@ -14,7 +14,6 @@ libyt-specific IO functions
 #-----------------------------------------------------------------------------
 
 import numpy as np
-from itertools import groupby
 
 from yt.utilities.io_handler import \
     BaseIOHandler
@@ -41,11 +40,8 @@ class IOHandlerlibyt(BaseIOHandler):
 #                        slice(ghost_zones,-ghost_zones),
 #                        slice(ghost_zones,-ghost_zones))
 
-
     def _read_particle_coords(self, chunks, ptf):
         chunks = list(chunks)
-
-        mylog.debug("self.grid_data.keys() = %s", self.grid_data.keys())
 
         for chunk in chunks:
             for g in chunk.objs:
@@ -68,7 +64,6 @@ class IOHandlerlibyt(BaseIOHandler):
                         continue
                     else:
                         yield ptype, (x, y, z)
-
 
     def _read_particle_fields(self, chunks, ptf, selector):
         chunks = list(chunks)
@@ -131,42 +126,11 @@ class IOHandlerlibyt(BaseIOHandler):
         # If no more fluid fields to read, return rv. Or else, read fluid fields
         if len(fluid_fields) == 0:
             return rv
-        field_list = self.param_yt["field_list"]
+
         for field in fluid_fields:
             ftype, fname = field
             for g in chunk.objs:
-                if field_list[fname]["field_define_type"] == "cell-centered":
-                    data_convert = self.grid_data[g.id][fname][:, :, :]
-                elif field_list[fname]["field_define_type"] == "face-centered":
-                    # Convert face-centered to cell-centered
-                    data_temp = self.grid_data[g.id][fname]
-                    grid_dim = self.hierarchy["grid_dimensions"][g.id]
-                    axis = np.argwhere(grid_dim != data_temp.shape)
-                    assert len(axis) == 1, \
-                        "Field [ %s ] is not a face-centered data, " \
-                        "grid_dimensions = %s, field data dimensions = %s" % (fname, grid_dim, (data_temp.shape,))
-                    assert data_temp.shape[axis[0, 0]] - 1 == grid_dim[axis[0, 0]], \
-                        "Field [ %s ] is not a face-centered data, " \
-                        "grid_dimensions = %s, field data dimensions = %s" % (fname, grid_dim, (data_temp.shape,))
-                    if axis == 0:
-                        data_convert = 0.5 * (data_temp[:-1, :, :] + data_temp[1:, :, :])
-                    if axis == 1:
-                        data_convert = 0.5 * (data_temp[:, :-1, :] + data_temp[:, 1:, :])
-                    if axis == 2:
-                        data_convert = 0.5 * (data_temp[:, :, :-1] + data_temp[:, :, 1:])
-                elif field_list[fname]["field_define_type"] == "derived_func":
-                    data_convert = self.libyt.derived_func(g.id, fname)
-                else:
-                    # Since we only supports "cell-centered", "face-centered", "derived_func" tags for now
-                    # Raise an error if enter this block.
-                    raise ValueError("libyt does not have field_define_type [ %s ]" %
-                                     (field_list[fname]["field_define_type"]))
-
-                # Swap axes or not
-                if field_list[fname]["swap_axes"] is True:
-                    data_view = data_convert.swapaxes(0, 2)
-
-                rv[g.id][field] = data_view
+                rv[g.id][field] = self._get_data_from_libyt(g, fname)
         return rv
 
     def _read_fluid_selection(self, chunks, selector, fields, size):
@@ -176,30 +140,25 @@ class IOHandlerlibyt(BaseIOHandler):
         mylog.debug("fields = %s", fields)
 
         mylog.debug("self.param_yt['field_list'] = %s", self.param_yt["field_list"])
-        field_list = self.param_yt["field_list"]
         rv = {}
         chunks = list(chunks)
 
-        # TODO: Need careful check for this if block
         if selector.__class__.__name__ == "GridSelector":
             if not (len(chunks) == len(chunks[0].objs) == 1):
-                raise RuntimeError
+                raise RuntimeError("class IOHandlerlibyt, def _read_fluid_selection, selector == GridSelector, "
+                                   "chunk to be read not equal to 1.")
             g = chunks[0].objs[0]
             for ftype, fname in fields:
-                rv[(ftype, fname)] = self.grid_data[g.id][fname].swapaxes(0, 2)
+                rv[(ftype, fname)] = self._get_data_from_libyt(g, fname)
 
             mylog.debug("###### (class IOHandlerlibyt, def _read_fluid_selection)")
-
             return rv
 
-        # TODO: Need careful check for this if block
         if size is None:
             size = sum((g.count(selector) for chunk in chunks for g in chunk.objs))
 
         for field in fields:
-            ftype, fname = field
-            fsize = size
-            rv[field] = np.empty(fsize, dtype=self._field_dtype)
+            rv[field] = np.empty(size, dtype=self._field_dtype)
 
         ng = sum(len(c.objs) for c in chunks)
         mylog.debug("Reading %s cells of %s fields in %s grids",
@@ -211,45 +170,50 @@ class IOHandlerlibyt(BaseIOHandler):
             mylog.debug("ftype, fname = %s", field)
             for chunk in chunks:
                 for g in chunk.objs:
-                    ### for ghost_zones != 0
-                    #                   data_view = self.grid_data[g.id][fname][self.my_slice].swapaxes(0,2)
-                    # TODO: self.grid_data has all the g.id as keys, so we probably need additional check to prevent
-                    #       getting None object, which means current rank does not have the grid.
-                    if field_list[fname]["field_define_type"] == "cell-centered":
-                        mylog.debug("self.grid_data[g.id][fname].shape = %s", self.grid_data[g.id][fname].shape)
-                        data_convert = self.grid_data[g.id][fname][:, :, :]
-                    elif field_list[fname]["field_define_type"] == "face-centered":
-                        # convert to cell-centered
-                        data_temp = self.grid_data[g.id][fname]
-                        grid_dim = self.hierarchy["grid_dimensions"][g.id]
-                        axis = np.argwhere(grid_dim != data_temp.shape)
-                        assert len(axis) == 1, \
-                            "Field [ %s ] is not a face-centered data, " \
-                            "grid_dimensions = %s, field data dimensions = %s" % (fname, grid_dim, (data_temp.shape, ))
-                        assert data_temp.shape[axis[0, 0]] - 1 == grid_dim[axis[0, 0]], \
-                            "Field [ %s ] is not a face-centered data, " \
-                            "grid_dimensions = %s, field data dimensions = %s" % (fname, grid_dim, (data_temp.shape, ))
-                        if axis == 0:
-                            data_convert = 0.5 * (data_temp[:-1, :, :] + data_temp[1:, :, :])
-                        if axis == 1:
-                            data_convert = 0.5 * (data_temp[:, :-1, :] + data_temp[:, 1:, :])
-                        if axis == 2:
-                            data_convert = 0.5 * (data_temp[:, :, :-1] + data_temp[:, :, 1:])
-                    elif field_list[fname]["field_define_type"] == "derived_func":
-                        data_convert = self.libyt.derived_func(g.id, fname)
-                    else:
-                        # Since we only supports "cell-centered", "face-centered", "derived_func" tags for now
-                        # Raise an error if enter this block.
-                        raise ValueError("libyt does not have field_define_type [ %s ]" %
-                                         (field_list[fname]["field_define_type"]))
-
-                    # Swap axes or not
-                    if field_list[fname]["swap_axes"] is True:
-                        data_view = data_convert.swapaxes(0, 2)
-
+                    data_view = self._get_data_from_libyt(g, fname)
                     offset += g.select(selector, data_view, rv[field], offset)
-        assert (offset == fsize)
+            assert (offset == size)
 
         mylog.debug("###### (class IOHandlerlibyt, def _read_fluid_selection)")
 
         return rv
+
+    def _get_data_from_libyt(self, grid, fname):
+        # TODO: self.grid_data has all the g.id as keys, so we probably need additional check to prevent
+        #       getting None object, which means current rank does not have the grid.
+        field_list = self.param_yt["field_list"]
+        if field_list[fname]["field_define_type"] == "cell-centered":
+            mylog.debug("self.grid_data[g.id][fname].shape = %s", self.grid_data[grid.id][fname].shape)
+            data_convert = self.grid_data[grid.id][fname][:, :, :]
+        elif field_list[fname]["field_define_type"] == "face-centered":
+            # convert to cell-centered
+            data_temp = self.grid_data[grid.id][fname]
+            grid_dim = self.hierarchy["grid_dimensions"][grid.id]
+            if field_list[fname]["swap_axes"] is True:
+                grid_dim = np.flip(grid_dim)
+            axis = np.argwhere(grid_dim != data_temp.shape)
+            assert len(axis) == 1, \
+                "Field [ %s ] is not a face-centered data, " \
+                "grid_dimensions = %s, field data dimensions = %s, considering swap_axes" % (fname, grid_dim, (data_temp.shape,))
+            assert data_temp.shape[axis[0, 0]] - 1 == grid_dim[axis[0, 0]], \
+                "Field [ %s ] is not a face-centered data, " \
+                "grid_dimensions = %s, field data dimensions = %s, considering swap_axes" % (fname, grid_dim, (data_temp.shape,))
+            if axis == 0:
+                data_convert = 0.5 * (data_temp[:-1, :, :] + data_temp[1:, :, :])
+            elif axis == 1:
+                data_convert = 0.5 * (data_temp[:, :-1, :] + data_temp[:, 1:, :])
+            elif axis == 2:
+                data_convert = 0.5 * (data_temp[:, :, :-1] + data_temp[:, :, 1:])
+        elif field_list[fname]["field_define_type"] == "derived_func":
+            data_convert = self.libyt.derived_func(grid.id, fname)
+        else:
+            # Since we only supports "cell-centered", "face-centered", "derived_func" tags for now
+            # Raise an error if enter this block.
+            raise ValueError("libyt does not have field_define_type [ %s ]" %
+                             (field_list[fname]["field_define_type"]))
+
+        # Swap axes or not, then return
+        if field_list[fname]["swap_axes"] is True:
+            return data_convert.swapaxes(0, 2)
+        else:
+            return data_convert
