b41a8ea650086222443231289d0d0f011f3f7fbe
Matthew Turk <matthewturk@gmail.com>
2023-05-08 12:12:46 -0500

    Add stubs

--- yt_libyt/__init__.py
+++ yt_libyt/__init__.py
@@ -12,3 +12,13 @@ API for yt.frontends.libyt
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
+"""Top-level package for yt_libyt."""
+
+__author__ = """Matthew Turk"""
+__email__ = 'matthewturk@gmail.com'
+__version__ = '0.1.0'
+
+from .data_structures import libytDataset, libytGrid, libytHierarchy
+from .fields import libytFieldInfo
+from .io import libytIOHandler
+
