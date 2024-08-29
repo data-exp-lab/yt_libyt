f8ed81e82746b16afd78bc3e774e4e81378e5dc6
cindytsai <turquoisea.tsai@gmail.com>
2024-08-29 10:24:09 -0500

    Remove placeholder in tests.

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,18 +1,6 @@
-import pytest
-
-
 def test_import():
     import yt_libyt
 
     api_list = ["libytDataset", "libytGrid", "libytHierarchy", "libytFieldInfo", "libytIOHandler"]
     for attr in api_list:
         assert hasattr(yt_libyt, attr) is True, f"No class named {attr} in yt_libyt"
-
-
-def test_always_pass():
-    assert True
-
-
-@pytest.mark.xfail
-def test_expected_failure():
-    assert 1 == 2
